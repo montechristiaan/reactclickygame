3772d268933eafd6c7d4822db16c5408eb7513df
montechristiaan <christiaan.simmons@live.com>
2019-06-10 20:38:04 -0500

    Updates

---
+++ precache-manifest.803d4bfe82f63f8abb6347972842a6a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84d8b4d7fe690e027e4060aa49cbed1",
+    "url": "/reactclickygame/index.html"
+  },
+  {
+    "revision": "7a95d55ad76fe49f7994",
+    "url": "/reactclickygame/static/css/main.65e0de8c.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/reactclickygame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "7a95d55ad76fe49f7994",
+    "url": "/reactclickygame/static/js/main.e8b733f8.chunk.js"
+  },
+  {
+    "revision": "3bda9a3765674e2eb487",
+    "url": "/reactclickygame/static/js/runtime~main.af3f12b9.js"
+  }
+]);
